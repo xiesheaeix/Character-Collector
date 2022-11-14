fc284042fcb154ce43a6634848cc513cecfb6af8
xiesheaeix <sheae123@gmail.com>
2022-11-14 13:53:12 -0800

    about and character routes

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('characters/',views.characters_index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
 
+# delete later when we add data to db
+characters = [
+  {'name': 'SpongeBob SquarePants', 'description': 'Sponge', 'starredin': 'SpongeBob SquarePants'},
+  {'name': 'Bugs Bunny', 'description': 'Rabbit', 'starredin': 'Looney Tunes'},
+  {'name': 'Stewie Griffin', 'description': 'Evil Genius Baby', 'starredin': 'Family Guy'},
+  {'name': 'Scooby Doo', 'description': 'Dog', 'starredin': 'Scooby Doo'},
+
+]
+
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def characters_index(request):
+    return render(request, 'characters/index.html', {
+        'characters': characters
+    })
