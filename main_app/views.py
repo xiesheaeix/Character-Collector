fe42e99a220a574cd124741fc32037817a9211a2
xiesheaeix <sheae123@gmail.com>
2022-11-14 15:38:16 -0800

    navbar and character page style

--- main_app/views.py
+++ main_app/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 # delete later when we add data to db
 characters = [
-  {'name': 'SpongeBob SquarePants', 'description': 'Sponge', 'starredin': 'SpongeBob SquarePants'},
-  {'name': 'Bugs Bunny', 'description': 'Rabbit', 'starredin': 'Looney Tunes'},
-  {'name': 'Stewie Griffin', 'description': 'Evil Genius Baby', 'starredin': 'Family Guy'},
-  {'name': 'Scooby Doo', 'description': 'Dog', 'starredin': 'Scooby Doo'},
+  {'name': 'SpongeBob SquarePants', 'description': 'Sponge', 'starred_in': 'SpongeBob SquarePants', },
+  {'name': 'Bugs Bunny', 'description': 'Rabbit', 'starred_in': 'Looney Tunes', },
+  {'name': 'Stewie Griffin', 'description': 'Evil Genius Baby', 'starred_in': 'Family Guy'},
+  {'name': 'Scooby Doo', 'description': 'Dog', 'starred_in': 'Scooby Doo'},
 
 ]
 
